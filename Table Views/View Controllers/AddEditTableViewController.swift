de8a08fae44e7ca018591b3f9003ecb96646ea05
JohnKostanov <kostanovd@icloud.com>
2019-07-30 21:24:49 +0200

    Add custom cell and fix constraint

---
+++ Table Views/View Controllers/AddEditTableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AddEditTableViewController.swift
+//  Table Views
+//
+//  Created by  Джон Костанов on 30/07/2019.
+//  Copyright © 2019 John Kostanov. All rights reserved.
+//
+
+import UIKit
+
+class AddEditTableViewController: UITableViewController {
+
+    
+}
